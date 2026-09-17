1839fd32e84d33ff81085bf3367c23d5bb094212
Vanilla <chuanshern@gmail.com>
2026-09-18 06:04:09 +0800

    fallback

--- lib/models/route.dart
+++ lib/models/route.dart
@@ -6,6 +6,7 @@ class GtfsRoute {
   final int type;
   final String color;
   final String textColor;
+  final String feedCategory;
 
   GtfsRoute({
     required this.id,
@@ -15,5 +16,21 @@ class GtfsRoute {
     required this.type,
     required this.color,
     required this.textColor,
+    this.feedCategory = '',
   });
+
+  String get uniqueKey => '$feedCategory|$id';
+
+  factory GtfsRoute.fromMap(Map<String, dynamic> map) {
+    return GtfsRoute(
+      id: map['route_id']?.toString() ?? '',
+      agencyId: map['agency_id']?.toString() ?? '',
+      shortName: map['route_short_name']?.toString() ?? '',
+      longName: map['route_long_name']?.toString() ?? '',
+      type: int.tryParse(map['route_type']?.toString() ?? '') ?? 0,
+      color: map['route_color']?.toString() ?? '',
+      textColor: map['route_text_color']?.toString() ?? '',
+      feedCategory: map['feed_category']?.toString() ?? '',
+    );
+  }
 }
